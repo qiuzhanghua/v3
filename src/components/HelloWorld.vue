23c8fe3b497c61d10a21502462c7ed4a8b76ac6a
qiuzhanghua <qiuzhanghua@icloud.com>
2021-01-20 21:58:04 +0800

    Vue.js with tailwind

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+div {
+  @apply p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4;
+}
+</style>
